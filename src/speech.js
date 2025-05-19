4bdf7e1642a157940c440b002c255581d720d1a4
Matthew <107056920+s023322@users.noreply.github.com>
2025-05-18 20:29:44 -0400

    Add files via upload

---
+++ src/speech.js
@@ -0,0 +1,23 @@
+function speak() {
+  let textElement = document.getElementById("text");
+  let text = document.getElementById("inputText").value || "test";
+  let rate = document.getElementById("readingSpeed").value || 5;
+
+  if ("speechSynthesis" in window) {
+    const speech = new SpeechSynthesisUtterance();
+    speech.text = text;
+
+    const voices = window.speechSynthesis.getVoices();
+    speech.voice = voices[0];
+    speech.rate = rate;
+    speech.pitch = 0.1;
+    speech.volume = 1;
+    speech.lang = "en-US";
+    window.speechSynthesis.speak(speech);
+
+    speech.addEventListener("boundary", ({ charIndex, charLength }) => {
+      const word = text.slice(charIndex, charIndex + charLength);
+      textElement.innerHTML = word;
+    });
+  }
+}
